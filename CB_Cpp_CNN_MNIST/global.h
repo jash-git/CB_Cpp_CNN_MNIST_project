2a4ac99ae6f2e7952026a3b102102deb01f123cc
jash.liao <jash.liao@gmail.com>
2022-03-31 15:15:07 +0800

    Create

---
+++ CB_Cpp_CNN_MNIST/global.h
@@ -0,0 +1,20 @@
+#ifndef GLOBAL_H_INCLUDED
+#define GLOBAL_H_INCLUDED
+
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+
+#define UCHR unsigned char
+#define UINT unsigned int
+
+const UINT g_cImageSize = 28;
+const UINT g_cVectorSize = 29;
+const UINT g_cOutputSize = 10;
+//const UINT g_cCountTrainingSample = 60000;
+//const UINT g_cCountTestingSample = 10000;
+const UINT g_cCountTrainingSample = 60000;
+const UINT g_cCountTestingSample = 10000;
+
+#endif // GLOBAL_H_INCLUDED
---
+++ Readme.txt
@@ -0,0 +1,5 @@
+C++ CNN MNIST
+
+資料來源: https://github.com/cooparation/cnnMNIST
+
+改成Code::Blocks專案，可以正確編譯成功，但是沒有實際做後續測試與驗證
